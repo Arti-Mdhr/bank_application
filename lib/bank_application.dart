f8b6d5d6d692fdc498700c08b2babdc6a02822ee
Arti-Mdhr <artimanandhar@gmail.com>
2025-11-08 14:52:48 +0545

    final commit

--- lib/bank_application.dart
+++ lib/bank_application.dart
@@ -1,157 +1,170 @@
 abstract class BankAccount {
-  int _accountNumber;
-  String _accountHolderName;
-  double _balance;
+    int _accountNumber;
+    String _accountHolderName;
+    double _balance;
 
-  List<String> _transactions = [];
+    List <String> _transactions = [];
 
-  void withdraw(double amount);
-  void deposit(double amount);
+    void withdraw(double amount);
+    void deposit (double amount);
 
   BankAccount(
-    this._accountNumber,
-    this._accountHolderName,
-    this._balance,
-  );
+     this._accountNumber, 
+     this._accountHolderName, 
+     this._balance 
+    );
 
-  get getAccNum => _accountNumber;
-
-  get getAccHolName => _accountHolderName;
-
-  get getBalance => _balance;
-
-  set setAccNum(int accNum) {
-    _accountNumber = accNum;
+  get getAccNum{
+    return _accountNumber;
   }
 
-  set setAccHolName(String accName) {
-    _accountHolderName = accName;
+  get getAccHolName{
+    return _accountHolderName;
   }
 
-  set setBalance(double newbalance) {
-    if (newbalance <= 0) {
-      print("Balance cannot be negative or zero!");
-    } else {
-      _balance = newbalance;
+  get getBalance{
+    return _balance;
+  }
+
+  set setAccNum(int accNum){
+    _accountNumber=accNum;
+  }
+
+  set setAccHolName(String accName){
+    _accountHolderName=accName;
+  }
+
+  set setBalance(double newbalance){
+    if(newbalance<=0){
+      print("Balance cannot be negative or zero!!!");
+    }else{
+      _balance=newbalance;
     }
   }
 
-  void displayInfo() {
+  void displayInfo(){
     print('''
     Account Number: $_accountNumber
     Account Holder Name: $_accountHolderName
     Balance: Rs. $_balance
     ''');
+    
   }
 
-  void addTransaction(String detail) {
+  void addTransaction(String detail){
     _transactions.add(detail);
   }
 
-  void viewTransactions() {
-    print("\nTransaction History for $_accountNumber:\n");
-    for (var t in _transactions) {
+  void viewTransactions(){
+    print("\nTransaction History for $_accountNumber is: \n");
+    for(var t in _transactions){
       print(t);
     }
   }
+
 }
 
-abstract class InterestBearing extends BankAccount {
+abstract class InterestBearing extends BankAccount{
   InterestBearing(super._accountNumber, super._accountHolderName, super._balance);
 
   double calculateInterest();
 }
 
-class SavingsAccount extends InterestBearing {
-  int numOfWithdraw = 0;
-  final int withdrawLimit = 3;
-  final double _minBalance = 500;
-  final double _interestRate = 0.02;
+class SavingsAccount extends InterestBearing{
+  int numOfWithdraw =0;
+  final int withdrawLimit=3;
+  final double _minBalance =500;
+  final double _interestRate=0.02;
 
-  SavingsAccount(super._accountNumber, super._accountHolderName, super._balance);
-
+  SavingsAccount(super._accountNumber,super._accountHolderName, super._balance);
+  
   @override
   void withdraw(double amount) {
-    if (numOfWithdraw >= withdrawLimit) {
-      print("Withdrawal limit reached!");
-      return;
+    if (numOfWithdraw>withdrawLimit) {
+      print("Withdraw Limit Reached !!");
     }
-    if (_balance - amount < _minBalance) {
-      print("Insufficient balance || Cannot withdraw below minimum balance");
-    } else {
+    if(_balance-amount< _minBalance){
+      print("Insufficient balance || Cannot Withdraw below minimum balance");
+
+    }else{
       _balance -= amount;
       numOfWithdraw++;
       addTransaction("Withdrawn Rs. $amount");
-      print("Withdrawn Rs. $amount from the account");
+      print("Withdrawn Rs. $amount from the acount");
     }
-  }
 
+  }
+ 
   @override
   void deposit(double amount) {
-    _balance += amount;
+    _balance +=amount;
     addTransaction("Deposited Rs. $amount");
     print('''Deposited Rs. $amount into the account
     New Balance: $_balance''');
   }
+  
 
-  @override
+ @override
   double calculateInterest() {
-    return _balance * _interestRate;
+    return _balance*_interestRate;
   }
 }
 
-class CheckingAccount extends BankAccount {
+class CheckingAccount extends BankAccount{
   final int _overdraftFee = 35;
   CheckingAccount(super._accountNumber, super._accountHolderName, super._balance);
-
-  @override
+   @override
   void withdraw(double amount) {
-    _balance -= amount;
-    if (_balance < 0) {
-      _balance -= _overdraftFee;
-      print("Overdraft occurred! Fee of Rs. $_overdraftFee applied");
+    _balance -=amount;
+    if(_balance<0){
+      _balance-=_overdraftFee;
+      print("Overdraft Discovered !! Fee of Rs. $_overdraftFee Applied");
     }
-    addTransaction("Withdrawn Rs. $amount");
-    print("Withdrawn Rs. $amount from the account");
+      addTransaction("Withdrwan Rs. $amount");
+      print("Withdrawn Rs. $amount from the acount");
   }
 
   @override
   void deposit(double amount) {
-    _balance += amount;
+    _balance +=amount;
     addTransaction("Deposited Rs. $amount");
     print("Deposited Rs. $amount into the account");
   }
+
 }
 
-class PremiumAccount extends InterestBearing {
-  final double _minBalance = 1000;
-  final double _interestRate = 0.05;
+class PremiumAccount extends InterestBearing{
+  final double _minBalance =1000;
+  final double _interestRate=0.05;
 
-  PremiumAccount(super._accountNumber, super._accountHolderName, super._balance);
-
+  PremiumAccount(super._accountNumber,super._accountHolderName, super._balance);
+  
   @override
   void withdraw(double amount) {
-    if (_balance - amount < _minBalance) {
-      print("Insufficient balance || Cannot withdraw below minimum balance");
-    } else {
+    if(_balance-amount< _minBalance){
+      print("Insufficient balance || Cannot Withdraw below minimum balance");
+
+    }else{
       _balance -= amount;
-      addTransaction("Withdrawn Rs. $amount");
-      print('''Withdrawn Rs. $amount from the account
+      addTransaction("Withdrwan Rs. $amount");
+      print('''Withdrawn Rs. $amount from the acount
       New Balance: Rs. $_balance''');
     }
-  }
 
+  }
+ 
   @override
   void deposit(double amount) {
-    _balance += amount;
+    _balance +=amount;
     addTransaction("Deposited Rs. $amount");
     print('''Deposited Rs. $amount into the account
     New Balance: Rs. $_balance''');
   }
+  
 
-  @override
+ @override
   double calculateInterest() {
-    return _balance * _interestRate;
+    return _balance*_interestRate;
   }
 }
 
@@ -166,7 +179,7 @@ class StudentAccount extends BankAccount {
       print("Insufficient balance");
     } else {
       _balance -= amount;
-      addTransaction("Withdrawn Rs. $amount");
+      addTransaction("Withdrwan Rs. $amount");
       print("Withdrawn Rs. $amount from the account. New Balance: $_balance");
     }
   }
@@ -179,45 +192,48 @@ class StudentAccount extends BankAccount {
     } else {
       _balance += amount;
       addTransaction("Deposited Rs. $amount");
-      print("Deposited Rs. $amount into account. New Balance: $_balance");
+       print("Deposited Rs. $amount into account. New Balance: $_balance");
     }
+   
   }
+  
 }
 
-class Bank {
-  List<BankAccount> accounts = [];
+class Bank{
+  List <BankAccount> accounts = [];
 
-  void addAccount(BankAccount account) {
+  void addAccount(BankAccount account){
     accounts.add(account);
-    print("Account for ${account.getAccHolName} created (Acc No: ${account.getAccNum})");
+    print("Account for ${account.getAccNum} created");
   }
 
-  BankAccount? findAccount(int accountNum) {
-    for (var account in accounts) {
-      if (account._accountNumber == accountNum) {
+  BankAccount? findAccount(int accountNum){
+    for (var account in accounts){
+      if (account._accountNumber == accountNum){
         return account;
       }
     }
-    print("Account with number $accountNum not found");
+    print("The account of account number $accountNum not found");
     return null;
   }
 
-  void transfer(int senderNum, int receiverNum, double amount) {
-    var sender = findAccount(senderNum);
-    var receiver = findAccount(receiverNum);
+  void transfer(int sendAccountNum, int receiveAccountNum, double amount){
+    var sendAccount =findAccount(sendAccountNum);
+    var receiveAccount = findAccount(receiveAccountNum);
 
-    if (sender == null || receiver == null) {
-      print("Transaction invalid: Account(s) not found");
+
+    if(sendAccount == null || receiveAccount== null){
+      print("Transaction Invalid: Account(s) not found");
       return;
     }
 
-    sender.withdraw(amount);
-    receiver.deposit(amount);
+    sendAccount.withdraw(amount);
+    receiveAccount.deposit(amount);
 
-    sender.addTransaction("Transferred Rs. $amount to Acc $receiverNum");
-    receiver.addTransaction("Received Rs. $amount from Acc $senderNum");
 
-    print("Transferred Rs. $amount from ${sender.getAccHolName} to ${receiver.getAccHolName}");
+    sendAccount.addTransaction("Transferred Rs. $amount to Acc $sendAccountNum");
+    receiveAccount.addTransaction("Received Rs. $amount from Acc $receiveAccountNum");
+    print("Transferred Rs. $amount from ${sendAccount.getAccHolName} to ${receiveAccount.getAccHolName}");
   }
 
   void applyMonthlyInterest() {
@@ -244,31 +260,34 @@ class Bank {
 void main(List<String> args) {
   Bank bank = Bank();
 
-  var sav = SavingsAccount(1101, "Aayush Sharma", 2500);
-  var prem = PremiumAccount(2202, "Kritika Rai", 80000);
-  var check = CheckingAccount(3303, "Milan Gurung", 5000);
-  var std = StudentAccount(1404, "Sneha Lama", 1500);
+  var sav = SavingsAccount(1001, "Sita Shrestha", 2000);
+  var prem = PremiumAccount(2002, "Hari Katwal", 500000);
+  var check = CheckingAccount(3003, "Manat ghale", 4000);
+  var std = StudentAccount(1234, "Susma Moktan", 2000);
 
   bank.addAccount(sav);
   bank.addAccount(prem);
   bank.addAccount(check);
   bank.addAccount(std);
 
-  sav.deposit(500);
-  sav.withdraw(800);
-  sav.withdraw(400);
 
-  check.withdraw(5200);
+  sav.deposit(200);
+  sav.withdraw(1700);
+  sav.withdraw(500);
 
-  std.deposit(3000);
-  std.withdraw(600);
 
-  prem.withdraw(12000);
+  check.withdraw(4000); 
+
+  std.deposit(2000);
+  std.withdraw(100);
+
+  prem.withdraw(6000);
 
   bank.applyMonthlyInterest();
 
-  bank.transfer(1101, 2202, 200);
-  bank.transfer(3303, 1404, 500);
+  bank.transfer(1001, 2002, 100);
+  bank.transfer(1002, 2002, 1000);
 
   bank.generateReport();
+
 }
